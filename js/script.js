8183dd1a3d9eafc47db239b934cfe6282f2ee61c
WarriorX623 <owenhwtam@gmail.com>
2021-03-02 13:00:27 +0800

    Animations + projects

--- js/script.js
+++ js/script.js
@@ -19,6 +19,11 @@ window.addEventListener("load", function() {
     const loader = document.querySelector(".loader");
     loader.className += " hidden";
 });
+gsap.registerPlugin(ScrollTrigger);
 
-
-
+var tl = gsap.timeline();
+var tl2 = gsap.timeline();
+tl.from(".banner-title .stag", {duration: 1, opacity: 0, x: -150, stagger: 0.2});
+tl.from(".banner-title .underline", {duration: 1.5, width: 0, ease: Elastic.easeOut});
+gsap.from(".contact-contact", {scrollTrigger: ".contact-contact", duration: 1, y: -50, opacity: 0 });
+gsap.from(".stagge", {scrollTrigger: ".contact-contact", duration: 1, opacity:0, x: -50, stagger: 0.2, delay: 0.9});
