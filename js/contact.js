6d582238161b0012d0821806d9ccef5fce5e3442
WarriorX623 <owenhwtam@gmail.com>
2021-04-08 21:43:35 +0800

    BIG UPDATE

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+function toggleMenu() {
+    if (menu.classList.contains("opened")) {
+        menu.classList.remove("opened");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+
+    }else {
+        menu.classList.add("opened");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+
+toggle.addEventListener('click', toggleMenu, false);
+
+
+window.addEventListener("load", function() {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
+});
