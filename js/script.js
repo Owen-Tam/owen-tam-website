a24ef5066ac82222439aef31344bf3c404bfca7b
WarriorX623 <owenhwtam@gmail.com>
2021-04-11 18:32:02 +0800

    Better mobile nav

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-const toggle = document.querySelector('.toggle');
-const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll(".nav-links li");
 
-function toggleMenu() {
-    if (menu.classList.contains("opened")) {
-        menu.classList.remove("opened");
-        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
 
-    }else {
-        menu.classList.add("opened");
-        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-    }
-}
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    hamburger.classList.toggle("toggle");
 
-toggle.addEventListener('click', toggleMenu, false);
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+});
 
 
 window.addEventListener("load", function() {
@@ -21,27 +19,28 @@ window.addEventListener("load", function() {
 });
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".banner-h", {x:-200, duration: 1, ease: "power1"});
-gsap.from(".banner-p", {x:200, duration: 1, ease: "power1"});
-gsap.from(".banner-btn", {x:-200, duration: 1, ease: "power1"});
-gsap.from(".services-p", {
-    scrollTrigger :{
-        trigger: ".services-p",
-        toggleActions: "play pause resume pause"
-    }, x:200, duration: 0.7, ease: "power1"});
-gsap.from(".services-h", {
-    scrollTrigger :{
-        trigger: ".services-h",
-        toggleActions: "play pause resume pause"
-    }, x:-200, duration: 0.7, ease: "power1"});
-gsap.from(".projects-p", {
-    scrollTrigger :{
-        trigger: ".projects-p",
-        toggleActions: "play pause resume pause"
-    }, x:200, duration: 0.7, ease: "power1"});
-gsap.from(".projects-h", {
-    scrollTrigger :{
-        trigger: ".projects-h",
-        toggleActions: "play pause resume pause"
-    }, x:-200, duration: 0.7, ease: "power1"});
+// gsap.from(".banner-h", {x:-200, duration: 1, ease: "power1"});
+// gsap.from(".banner-p", {x:200, duration: 1, ease: "power1"});
+// gsap.from(".banner-btn", {x:-200, duration: 1, ease: "power1"});
+// gsap.from(".services-p", {
+//     scrollTrigger :{
+//         trigger: ".services-p",
+//         pinSpacing: "none",
+//         toggleActions: "play pause resume pause"
+//     }, x:200, duration: 0.7, ease: "power1"});
+// gsap.from(".services-h", {
+//     scrollTrigger :{
+//         trigger: ".services-h",
+//         toggleActions: "play pause resume pause"
+//     }, x:-200, duration: 0.7, ease: "power1"});
+// gsap.from(".projects-p", {
+//     scrollTrigger :{
+//         trigger: ".projects-p",
+//         toggleActions: "play pause resume pause"
+//     }, x:200, duration: 0.7, ease: "power1"});
+// gsap.from(".projects-h", {
+//     scrollTrigger :{
+//         trigger: ".projects-h",
+//         toggleActions: "play pause resume pause"
+//     }, x:-200, duration: 0.7, ease: "power1"});
     
