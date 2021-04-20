bad036eeb392157ae3116469e2d40cea80a73e19
WarriorX623 <owenhwtam@gmail.com>
2021-04-20 21:25:04 +0800

    About page half finished

---
+++ js/about.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  hamburger.classList.toggle("toggle");
+
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
+
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loader");
+  loader.className += " hidden";
+});
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.from(".about-p", { x: -200, duration: 0.7, ease: "power1" });
+gsap.from(".about-h", { x: 200, duration: 0.7, ease: "power1" });
